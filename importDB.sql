e256bc3053b80b1aeae0b59a33ea70c02ab6243e
Genesis30 <carlos.bevia@gmail.com>
2014-03-18 11:33:43 +0100

    Delete importDB.sql

--- importDB.sql
+++
@@ -1,7 +0,0 @@
-USE recetaMedica;
-
-LOAD DATA LOCAL INFILE '/home/genesis/Desktop/medicamentos.csv'
-INTO TABLE tablaMedicamentos
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-(codigoNacional, nombreProducto, tipoFarmaco, nombreGenericoEfecto, codigoLaboratorio, nombreLaboratorio, estado, fechaAlta, fechaBaja, aportacionBeneficiario, principioActivo, precioIva, precioReferencia, precioAgrupacion, nombreAgrupacion, diagnosticoHospitalario, largaDuracion, controlMedico, medicamentoHuerfano, deduccionDecreto);
